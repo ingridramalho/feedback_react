273ebff982f49289aca658dd262e13da86881eb7
ingridrsantosdejesus <ingridrsantosdejesus@gmail.com>
2025-04-01 00:45:47 +0000

    fim da criação e estilização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import iconStar from "./images/icon-star.svg"
+import illustrationThankYou from"./images/illustration-thank-you.svg"
 
 export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
@@ -41,7 +42,12 @@ export function App() {
     </div>
     ) : (
       <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
-        <p>{notaAvaliacao}</p>
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
+
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
+
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
+        <p className="text-light-grey text-sm leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
       </div>
     )
   )
